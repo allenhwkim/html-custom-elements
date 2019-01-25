5b1b2b7b991b16ffb8b814e384f5de910c39bde8
Allen Kim <allenhwkim@gmail.com>
2019-01-24 21:40:04 -0500

    update with new demos and home page

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,44 @@
 import '../src';
+import Prism from 'prismjs';
 
+window.showCode = function(htmlId, jsId, cssId) {
+  const el = document.createElement('div');
+  el.insertAdjacentHTML('beforeend', `
+    <hce-tabs class="code">
+      <div class="tabs">
+        <i tab-for="html">html</i>
+        <i tab-for="js">js</i>
+        <i tab-for="css">css</i>
+      </div>
+      <div class="contents" style="background:#f8f8f8"> 
+        <div contents-for="html"><pre></pre></div>
+        <div contents-for="js"><pre></pre></div>
+        <div contents-for="css"><pre></pre></div>
+      </div>
+    </hce-tabs>`);
+  document.currentScript.insertAdjacentElement('afterend', el);
 
+  function fillCode(id, type) {
+    let srcEl, dstEl, html;
+    if (id) {
+      srcEl = document.getElementById(id);
+      dstEl = el.querySelector(`[contents-for=${type}] pre`);
+      const lang = type === 'js' ? 'javascript': type;
+      html = Prism.highlight(srcEl.outerHTML, Prism.languages[lang], lang);
+      dstEl.innerHTML = html;
+    } else {
+      el.querySelector(`[tab-for=${type}]`).remove();
+      el.querySelector(`[contents-for=${type}]`).remove();
+    }
+  }
+
+  fillCode(htmlId, 'html');
+  fillCode(jsId, 'js');
+  fillCode(cssId, 'css');
+}
+
+window.highlight = function(lang = 'javascript') {
+  const codeEl = document.currentScript.previousElementSibling;
+  const html = Prism.highlight(codeEl.innerHTML, Prism.languages[lang], lang);
+  codeEl.innerHTML = html;
+}
--- src/carousel/carousel.js
+++ src/carousel/carousel.js
@@ -4,11 +4,11 @@ import css from './carousel.css';
 
 const html = `
   <div class="prev button-container">
-    <button (click)="show('prev')">&lt;</button>
+    <button (click)="show(index-1)">&lt;</button>
   </div>
   <hce-content></hce-content>
   <div class="next button-container">
-    <button (click)="show('next')">&gt;</button>
+    <button (click)="show(index+1)">&gt;</button>
   </div>
 
   <!-- shortcuts for each item -->
@@ -17,6 +17,8 @@ const html = `
 function __addShortcuts(shortcutsEl, listEl) {
   for (let i=0; i < listEl.children.length; i++) {
     const liEl = listEl.children[i];
+    liEl.addEventListener('click', _ => this.show(liEl));
+
     const shortcut = document.createElement('li');
     shortcut.innerHTML = '&nbsp;';
     shortcut.setAttribute('tabindex', 0);
@@ -24,10 +26,9 @@ function __addShortcuts(shortcutsEl, listEl) {
 
     shortcut.addEventListener('click', _ => this.show(i));
     shortcut.addEventListener('keydown', event => {
-      console.log('this', this);
       (event.key === 'Enter') && this.show(i);
-      (event.key === 'ArrowRight') && this.show('next');
-      (event.key === 'ArrowLeft') && this.show('prev');
+      (event.key === 'ArrowRight') && this.show(this.index+1);
+      (event.key === 'ArrowLeft') && this.show(this.index-1);
     });
   }
 }
@@ -44,7 +45,7 @@ function __getIndex(all, item) {
 }
 
 export class HCECarousel extends HTMLCustomElement {
-  // selected                  // currently selected index
+  // selected                  // default selected index
   // listEl: HTMLElement       // list to scroll
   // shortcutsEl: HTMLElement  // list of shortcuts
   // inviewEl: Element         // currently visible element
@@ -53,6 +54,7 @@ export class HCECarousel extends HTMLCustomElement {
   connectedCallback() {
     this.renderWith(html, css).then(_ => {
       this.listEl = this.querySelector('ul:not(.shortcuts), ol, .list');
+      this.listEl.classList.add('carousel-list');
 
       this.shortcutsEl = this.querySelector('ul.shortcuts');
       __addShortcuts.bind(this)(this.shortcutsEl, this.listEl);
@@ -61,33 +63,27 @@ export class HCECarousel extends HTMLCustomElement {
     });
   }
 
-  show(what) { // 'prev', 'next', or index
-    let scrollToEl;
-    if (what === 'prev') {
-      scrollToEl = this.inviewEl.previousElementSibling || this.listEl.lastElementChild;
-    } else if (what === 'next') {
-      scrollToEl = this.inviewEl.nextElementSibling || this.listEl.firstElementChild;
-    } else {
-      scrollToEl = this.listEl.querySelector(`*:nth-child(${what + 1})`);
-    }
-    this.selected = __getIndex(this.listEl.children, scrollToEl);
-
+  show(what) { // index, or element
     const prevTabIndexedEl = this.listEl.querySelector('[tabindex]');
-
-    // show it
-    const inline = what == 'prev' ? 'end': what === 'next' ? 'start' : 'center';
-    setTimeout(_ => scrollToEl.scrollIntoView({behavior: 'smooth', inline: inline }) );
+    let scrollToEl = what;
+    if (typeof what === 'number') {
+      this.index = (this.listEl.children.length + what) % this.listEl.children.length;
+      scrollToEl = this.listEl.children[this.index];
+    }
+    this.index = __getIndex(this.listEl.children, scrollToEl);
+    setTimeout(_ => scrollToEl.scrollIntoView({behavior: 'smooth'}) );
 
     // set shortcuts
-    if (this.shortcutsEl){
+    if (this.shortcutsEl.offsetParent){ // if visible
       const prevActiveShortcut = this.shortcutsEl.querySelector('.active');
-      const shortcutEl = this.shortcutsEl.children[this.selected];
+      const shortcutEl = this.shortcutsEl.children[this.index];
       prevActiveShortcut && prevActiveShortcut.classList.remove('active');
       shortcutEl.classList.add('active');
       shortcutEl.focus();
     }
 
     // set tabindex for accessibility
+    console.log('prevTabIndexedEl', prevTabIndexedEl)
     prevTabIndexedEl && prevTabIndexedEl.removeAttribute('tabindex'); 
     scrollToEl.setAttribute('tabindex', 0);
 
