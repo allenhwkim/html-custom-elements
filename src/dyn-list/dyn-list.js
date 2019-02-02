4efc72d2821dcc4a6238dc39ac7ba238a12d21d2
Allen Kim <allenhwkim@gmail.com>
2019-02-01 23:37:10 -0500

    show html/js code for demo

--- src/dyn-list/dyn-list.js
+++ src/dyn-list/dyn-list.js
@@ -27,6 +27,7 @@ const css = `
     box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, .14),
       0px 1px 1px 0px rgba(0, 0, 0, .12), 
       0px 2px 1px -1px rgba(0, 0, 0, .4);
+    z-index: 1;
   }
 `;
 
@@ -46,7 +47,7 @@ export class HCEDynList extends HTMLCustomElement {
   connectedCallback() {
     const templateEl = this.children[0];
     this.template = templateEl && templateEl.outerHTML;
-    templateEl.remove();
+    templateEl.style.display = 'none';
     this.renderWith(html, css).then(_ => {
       if (this.visibleBy) {
         const source = this.getAttribute('[source]') || this.getAttribute('source');
